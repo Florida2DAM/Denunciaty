d6ea49a87792d2a53f6f9bb3b73c3138bd280da7
Ignacio Ferrer <nachopad31@hotmail.com>
2015-12-17 01:29:59 +0100

    Animación del Splash Screen.

--- app/src/main/java/com/denunciaty/denunciaty/Splash.java
+++ app/src/main/java/com/denunciaty/denunciaty/Splash.java
@@ -1,9 +1,12 @@
 package com.denunciaty.denunciaty;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends Activity {
 
@@ -11,6 +14,31 @@ public class Splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        TextView tV = (TextView) findViewById(R.id.textView);
+        Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.fade_in);
+
+        img.startAnimation(an);
+        tV.startAnimation(an);
+
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
 }
