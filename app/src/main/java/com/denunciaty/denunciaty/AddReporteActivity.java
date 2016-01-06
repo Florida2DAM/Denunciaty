0f40311e50d78d103ed617fd9d0092227b0d2919
Julian <jumofe95@gmail.com>
2016-01-06 20:56:23 +0100

    add reporte

---
+++ app/src/main/java/com/denunciaty/denunciaty/AddReporteActivity.java
@@ -0,0 +1,16 @@
+package com.denunciaty.denunciaty;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.denunciaty.denunciaty.R;
+
+public class AddReporteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reporte);
+    }
+
+}
