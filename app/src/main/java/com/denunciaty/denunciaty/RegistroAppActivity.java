df1d8d35a075026648da4905757b74f15886a5f6
andreaalfonsoo <andreaalfonsoo@gmail.com>
2016-01-11 16:32:56 +0100

    Interfaz pantalla registro app

---
+++ app/src/main/java/com/denunciaty/denunciaty/RegistroAppActivity.java
@@ -0,0 +1,60 @@
+package com.denunciaty.denunciaty;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class RegistroAppActivity extends ActionBarActivity {
+
+    ImageView imagen;
+    EditText usuario, nombre, apellidos, email;
+    Button añadir, submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_app);
+
+        //Imagen del usuario
+        imagen = (ImageView) findViewById(R.id.iv_avatar);
+
+        //EditText con datos del usuario
+        usuario = (EditText) findViewById(R.id.et_usuario);
+        nombre = (EditText) findViewById(R.id.et_nombre);
+        apellidos = (EditText) findViewById(R.id.et_apellidos);
+        email = (EditText) findViewById(R.id.et_email);
+
+        //Botón para añadir usuario
+        añadir = (Button) findViewById(R.id.bt_añadir);
+
+        //Botón para entrar
+        submit = (Button) findViewById(R.id.bt_submit);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registro_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
