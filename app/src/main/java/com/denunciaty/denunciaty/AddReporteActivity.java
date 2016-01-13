011f045df5a389df2d4d1dca21dbd982d6d4e508
Julian <jumofe95@gmail.com>
2016-01-13 20:27:55 +0100

    navigation drawer (errores)

--- app/src/main/java/com/denunciaty/denunciaty/AddReporteActivity.java
+++ app/src/main/java/com/denunciaty/denunciaty/AddReporteActivity.java
@@ -1,16 +1,33 @@
 package com.denunciaty.denunciaty;
 
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import com.denunciaty.denunciaty.R;
-
-public class AddReporteActivity extends Activity {
+public class AddReporteActivity extends AppCompatActivity implements NavigationDrawerCallbacks{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reporte);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+        NavigationDrawerFragment mNavigationDrawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
+
+        //set up the drawer
+        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
+
+        /*populate the navigation drawer
+        mnavigationDrawerFragment.setUserData("nombre", "correo", BitmapFactory.decodeResource(getResources()), R.drawable.avatar);*/
+
     }
 
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+
+    }
 }
