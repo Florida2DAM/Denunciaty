ec1482b94b72431407c05cd9d711fdb47bcdbf2e
Julian <jumofe95@gmail.com>
2016-01-26 15:55:41 +0100

    navigation drawer en misreportes

--- app/src/main/java/com/denunciaty/denunciaty/MisReportesActivity.java
+++ app/src/main/java/com/denunciaty/denunciaty/MisReportesActivity.java
@@ -2,6 +2,9 @@ package com.denunciaty.denunciaty;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,13 +17,27 @@ import com.denunciaty.denunciaty.JavaClasses.Reporte;
 
 import java.util.ArrayList;
 
-public class MisReportesActivity extends Activity {
+public class MisReportesActivity extends AppCompatActivity implements NavigationDrawerCallbacks {
     ListView lV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_reportes);
         lV = (ListView) findViewById(R.id.listView);
+
+        //navigation drawer
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+        NavigationDrawerFragment mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
+
+
+        //set up the drawer
+        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
+
+        /*populate the navigation drawer
+        mnavigationDrawerFragment.setUserData("nombre", "correo", BitmapFactory.decodeResource(getResources()), R.drawable.avatar);*/
+
+
 
         //Pruebas
         final ArrayList<Reporte> reportes= new ArrayList<Reporte>();
@@ -71,5 +88,10 @@ public class MisReportesActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+
     }
 }
