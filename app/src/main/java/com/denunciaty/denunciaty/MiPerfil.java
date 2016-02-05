6657abcd237436382d27afab83fe5d65b3626069
andreaalfonsoo <andreaalfonsoo@gmail.com>
2016-02-05 12:35:00 +0100

    Pantalla mi perfil

---
+++ app/src/main/java/com/denunciaty/denunciaty/MiPerfil.java
@@ -0,0 +1,171 @@
+package com.denunciaty.denunciaty;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MiPerfil extends AppCompatActivity {
+
+    EditText usuario, nombre, apellido, email, localidad;
+    ImageView foto;
+    ImageButton edita;
+    Button guardar, cambiar_contraseña;
+    String passInput = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_perfil);
+
+        usuario = (EditText) findViewById(R.id.et_usuario);
+        nombre = (EditText) findViewById(R.id.et_nombre);
+        apellido = (EditText) findViewById(R.id.et_apellidos);
+        email = (EditText) findViewById(R.id.et_email);
+        localidad = (EditText) findViewById(R.id.et_localidad);
+        foto = (ImageView) findViewById(R.id.iv_avatar);
+
+        edita = (ImageButton) findViewById(R.id.ib_edit);
+        edita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cambiar_contraseña.setVisibility(View.VISIBLE);
+                guardar.setVisibility(View.VISIBLE);
+                usuario.setEnabled(true);
+                nombre.setEnabled(true);
+                apellido.setEnabled(true);
+                email.setEnabled(true);
+                localidad.setEnabled(true);
+
+            }
+        });
+
+        guardar = (Button) findViewById(R.id.bt_guardar);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cambiar_contraseña.setVisibility(View.INVISIBLE);
+                guardar.setVisibility(View.INVISIBLE);
+                usuario.setEnabled(false);
+                nombre.setEnabled(false);
+                apellido.setEnabled(false);
+                email.setEnabled(false);
+                localidad.setEnabled(false);
+            }
+        });
+
+        cambiar_contraseña = (Button) findViewById(R.id.bt_cambia_password);
+        cambiar_contraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInputDialog();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_mi_perfil, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void showInputDialog(){
+        LayoutInflater layout = LayoutInflater.from(MiPerfil.this);
+        View view = layout.inflate(R.layout.contrasenya, null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MiPerfil.this);
+        alertDialog.setView(view);
+
+        final EditText passAntigua = (EditText) view.findViewById(R.id.et_passAntigua);
+        final EditText passNueva = (EditText) view.findViewById(R.id.et_passNueva);
+        final EditText passNuevaRepetida = (EditText) view.findViewById(R.id.et_passNuevaRepite);
+
+        alertDialog.setCancelable(true)
+                .setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        String antigua = passAntigua.getText().toString();
+                        String nueva = passNueva.getText().toString();
+                        String nuevaRepetida = passNuevaRepetida.getText().toString();
+
+                        if (antigua.isEmpty() || nueva.isEmpty() || nuevaRepetida.isEmpty()) {
+                            Toast.makeText(MiPerfil.this, "Hay campos vacíos", Toast.LENGTH_SHORT).show();
+                        } else {
+                            //Encriptacion pass
+                            try {
+                                passInput = SHA1(passAntigua.getText().toString());
+                                passInput = SHA1(passNueva.getText().toString());
+                                passInput = SHA1(passNuevaRepetida.getText().toString());
+                            } catch (NoSuchAlgorithmException e) {
+                                e.printStackTrace();
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                    }
+                })
+                .setNegativeButton(R.string.cancelar_registroapp, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(MiPerfil.this, R.string.cancelando_registroapp, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = alertDialog.create();
+        alert.show();
+    }
+
+    //Encriptación
+    private static String convertToHex(byte[] data) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < data.length; i++) {
+            int halfbyte = (data[i] >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do {
+                if ((0 <= halfbyte) && (halfbyte <= 9)) {
+                    buf.append((char) ('0' + halfbyte));
+                }
+                else {
+                    buf.append((char) ('a' + (halfbyte - 10)));
+                }
+                halfbyte = data[i] & 0x0F;
+            } while(two_halfs++ < 1);
+        }
+        return buf.toString();
+    }
+
+    public static String SHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] sha1hash = new byte[40];
+        md.update(text.getBytes("iso-8859-1"), 0, text.length());
+        sha1hash = md.digest();
+        return convertToHex(sha1hash);
+    }
+}
