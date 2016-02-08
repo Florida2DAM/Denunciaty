84622e78817fb0cd58083c5376f8730f0b93b8a1
Julian <jumofe95@gmail.com>
2016-02-08 20:00:45 +0100

    fragment addReporte

--- app/src/main/java/com/denunciaty/denunciaty/FragmentAddReporte.java
+++ app/src/main/java/com/denunciaty/denunciaty/FragmentAddReporte.java
@@ -1,13 +1,21 @@
 package com.denunciaty.denunciaty;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,12 +23,15 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.File;
+
 public class FragmentAddReporte extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
-    TextView mLatitudeText, mLongitudeText;
+    //TextView mLatitudeText, mLongitudeText;
     Spinner sp_tipo;
+    ImageView img_camara, img_cruz;
 
     public void onStart() {
         super.onStart();
@@ -43,12 +54,12 @@ public class FragmentAddReporte extends Fragment implements GoogleApiClient.Conn
                     .addApi(LocationServices.API)
                     .build();
         }
+
         onStart();
-        mLatitudeText = (TextView) view.findViewById(R.id.mLatitudeText);
-        mLongitudeText = (TextView) view.findViewById(R.id.mLongitudeText);
+        //mLatitudeText = (TextView) view.findViewById(R.id.mLatitudeText);
+        //mLongitudeText = (TextView) view.findViewById(R.id.mLongitudeText);
 
         sp_tipo = (Spinner) view.findViewById(R.id.sp_tipo);
-
         //Creamos el adaptador
         ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(), R.array.tipo_reporte, android.R.layout.simple_spinner_item);
         //Añadimos el layout para el menú
@@ -56,18 +67,67 @@ public class FragmentAddReporte extends Fragment implements GoogleApiClient.Conn
         //Le indicamos al spinner el adaptador a usar
         sp_tipo.setAdapter(adapter);
 
+
+        img_camara = (ImageView) view.findViewById(R.id.img_camara);
+        img_camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(
+                        android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                //Creamos una carpeta en la memeria del terminal
+                File imagesFolder = new File(
+                        Environment.getExternalStorageDirectory(), "Tutorialeshtml5");
+                imagesFolder.mkdirs();
+                //añadimos el nombre de la imagen
+                File image = new File(imagesFolder, "foto.jpg");
+                Uri uriSavedImage = Uri.fromFile(image);
+                //Le decimos al Intent que queremos grabar la imagen
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+                //Lanzamos la aplicacion de la camara con retorno (forResult)
+                startActivityForResult(cameraIntent, 1);
+            }
+        });
+
+/*      CERRAR EL FRAGMENT
+        img_cruz = (ImageView) view.findViewById(R.id.img_cruz);
+        img_cruz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+*/
+
         return view;
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            //Creamos un bitmap con la imagen recientemente
+            //almacenada en la memoria
+            Bitmap bMap = BitmapFactory.decodeFile(
+                    Environment.getExternalStorageDirectory()+
+                            "/Tutorialeshtml5/"+"foto.jpg");
+            //Añadimos el bitmap al imageView para
+            //mostrarlo por pantalla
+            img_camara.setImageBitmap(bMap);
+        }
+
     }
 
     @Override
     public void onConnected(Bundle bundle) {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        if (mLastLocation != null) {
+        /*if (mLastLocation != null) {
             mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
             mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
             Log.d("COOR", String.valueOf(mLastLocation.getLatitude() + " " + mLastLocation.getLongitude()));
-        }
+        }*/
     }
 
     @Override
